05be8f0e0360b29335d414470966fa1964da3959
Shirin <kamisatosaan>
2025-08-08 15:01:00 +0600

    qwerty

--- picture_generate.py
+++ picture_generate.py
@@ -7,10 +7,12 @@ pipe = StableDiffusionPipeline.from_pretrained(
 )
 pipe = pipe.to("cpu")
 
-prompt = (
-    "Tiny cute dragon with glowing wings sitting on a human hand, sunny daylight, "
-    "highly detailed, fantasy style, soft focus, photorealistic, concept art"
+prompt = (  
+    "Genshin Impact electro ninja, purple and black kimono, "  
+    "lightning patterns on sleeves, katana crackling with energy, "  
+    "dynamic mid-air pose, sakura petals swirling around, "  
+    "Inazuma city lights in background, anime cel-shaded"  
 )
 image = pipe(prompt).images[0]
 
-image.save("dragon.png")
+image.save("electro_ninja.png")
